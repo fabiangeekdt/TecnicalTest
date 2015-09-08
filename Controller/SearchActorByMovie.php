6a9437e02e85286bf48000476904808fe947a4d5
fabian <@geekdt.com>
2015-09-08 05:11:55 -0500

    adding the comments of whats next to do

--- Controller/SearchActorByMovie.php
+++ Controller/SearchActorByMovie.php
@@ -13,7 +13,14 @@
 	$persons = $tmdb->searchPerson($actor);
 	foreach($persons as $person){
 		echo '<li>'. $person->getName() .' (<a href="https://www.themoviedb.org/person/'. $person->getID() .'">'. $person->getID() .'</a>)</li>';
+		
+		//Get all the movie roles
+			//get all the movie tittles in an array
+				//Call a function for showin the tittles in chronological order (create a function that compares dates in another .php file)
+				//echo the result to the .html site
+			
 	}
+	
 		
 
 ?>

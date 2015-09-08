b92f0a0805058014acb6880407d9c8496d1ce109
fabian <@geekdt.com>
2015-09-08 02:45:36 -0500

    Addint THMD PHP Framework

--- Controller/SearchActorByMovie.php
+++ Controller/SearchActorByMovie.php
@@ -2,10 +2,20 @@
 	header('content-type: application/json; charset=utf-8');
 	header("access-control-allow-origin: *");
 	
-	include 'init.php';
-	echo 'error';	
+	include("init.php");
+	include("tmdb-api.php");
+	
+	$actor = $_POST['actor']; 
 	
 	
-	
+	//instantie TMDB API with my key API
+	$tmdb = new TMDB($apikey, 'en', true);
+	//search for the indicate person
+	$persons = $tmdb->searchPerson($actor);
+	foreach($persons as $person){
+		echo '<li>'. $person->getName() .' (<a href="https://www.themoviedb.org/person/'. $person->getID() .'">'. $person->getID() .'</a>)</li>';
+	}
+		
+
 ?>
 	
--- Controller/init.php
+++ Controller/init.php
@@ -1,4 +1,7 @@
 <?php
+	define('ROOT_DIR', dirname(__FILE__));
+	define('ROOT_URL', substr($_SERVER['PHP_SELF'], 0, - (strlen($_SERVER['SCRIPT_FILENAME']) - strlen(ROOT_DIR))));
+	
 	//the movieBD api Key
 	$apikey = "403e35b3672c94ac8a22ec88ffa2848d";	
 ?>

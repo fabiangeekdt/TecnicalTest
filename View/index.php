55b2a70f4f8edc833cf925e87b81312b95ce1ba9
fabian <@geekdt.com>
2015-09-09 01:09:41 -0500

    delete comments

--- View/index.php
+++ View/index.php
@@ -8,18 +8,6 @@
 		<script type="text/javascript" src="js/SearchMovies.js"></script>	
 	</head>
 	<body style="background-color:lightgrey; ">
-		<!--
-		<center>
-			<form method="post" id="search_form" >
-				<label style="font-size:300%">ACTOR's WEBSEARCH</label>
-				</br></br>
-				<label for="actor">Actor's name:</label>
-				<input type="text" name="actor" id="actor"  placeholder="type the name of the actor you want to search."</input>
-				</br></br>
-				<input type="submit" id="search_movies" value="Search"</input>
-			</form>
-		</center>
-		-->
 		<?php
 			include("Viewer.php");
 			$init_view = new Viewer();

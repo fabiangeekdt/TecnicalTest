2e4f9e77f112444e7dc0f868956372fea8ac5b51
fabian <@geekdt.com>
2015-09-08 19:08:13 -0500

    obtaining the tittles and release date into an array (dictionary) for ordering it

--- Controller/SearchActorByMovie.php
+++ Controller/SearchActorByMovie.php
@@ -1,27 +1,45 @@
 <?php
 	header("access-control-allow-origin: *");
-	
+
 	include("init.php");
 	include("tmdb-api.php");
 	
+	//declaring variables
 	$actor = $_POST['actor']; 
-	
+	$actorid;
+	$movieRoles = array();
+	$arr_actedmovies = array();
+	$arr2 = array();
 	
 	//instantie TMDB API with my key API
 	$tmdb = new TMDB($apikey, 'en', true);
 	//search for the indicate person
 	$persons = $tmdb->searchPerson($actor);
+	
+	//look for the searched person in the array
 	foreach($persons as $person){
-		echo '<li>'. $person->getName() .' (<a href="https://www.themoviedb.org/person/'. $person->getID() .'">'. $person->getID() .'</a>)</li>';
-		
-		//Get all the movie roles
-			//get all the movie tittles in an array
-				//Call a function for showin the tittles in chronological order (create a function that compares dates in another .php file)
-				//echo the result to the .html site
-			
+		//Get the Actor ID
+		$actorid = $person->getID();
 	}
 	
-		
-
-?>
+	//get the Actor 
+	$person = $tmdb->getPerson($actorid);		
+	//Get all the movie roles
+	$movieRoles = $person->getMovieRoles();		
+	//get all the movies tittle and release date in an array
+	foreach($movieRoles as $movieRole){
+		//save records in an array
+		$arr2["movietitle"] = $movieRole->getMovieTitle();
+		$arr2["releasedate"] = $movieRole->getMovieReleaseDate();
+		array_push($arr_actedmovies, $arr2); 
+	}
 	
+	echo var_dump($arr_actedmovies);
+
+			//Call a function for showin the tittles in chronological order (create a function that compares dates in another .php file)
+			//echo the result to the .html site
+			
+	//to remember
+	//echo '<li>'. $movieRole->getMovieTitle() .' (realese: '. $movieRole->getMovieReleaseDate() .')</li>'; - line 34
+	//echo '<li>'. $person->getName() .' (<a href="https://www.themoviedb.org/person/'. $person->getID() .'">'. $person->getID() .'</a>)</li>'; - line 21
+?>

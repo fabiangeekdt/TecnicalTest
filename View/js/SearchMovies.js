64a012348438d715c5d73bd4edd544e643e7b47d
fabian <@geekdt.com>
2015-09-09 01:05:34 -0500

    fix regex expression

--- View/js/SearchMovies.js
+++ View/js/SearchMovies.js
@@ -2,13 +2,22 @@ var request;
 
 $(document).ready(function(){
 	$('#search_movies').click(function(e){
+		
+		//stop the form from being submitted
+		e.preventDefault();
+		
 		//declare the variables send from the page
 		var actor = $('#actor').val();
 		var error = false;
 		
 		if(actor.length == 0)
 			error = true;
-			
+
+		var patt1 = /\d/g;
+		var result = actor.match(patt1);			
+		if(result != null)
+			error = true;
+						
 		//Validate if there's empty variables	
 		if(error == false){
 			//disable the Search button to avoid spamming
@@ -31,8 +40,7 @@ $(document).ready(function(){
  					window.alert("jqXHR: " + jqXHR + "textStatus: " + textStatus + "err: " + errorThrown);
 				}
 			});
-			//stop the form from being submitted
-			e.preventDefault();
+
 			
 		}
 		if(error == true){
